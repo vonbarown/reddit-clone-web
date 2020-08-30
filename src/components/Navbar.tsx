14179f57872985780a0be5fb05b31c8330338ad2
vonbarown <vonielbrown@pursuit.org>
2020-08-30 00:04:55 -0400

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={4}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4} w="100%">
+      <Box>{body}</Box>
+    </Flex>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { NavBar } from "../components/Navbar";
 const Index = () => (
   <Container>
     <DarkModeSwitch />
+    <NavBar />
   </Container>
 );
 
